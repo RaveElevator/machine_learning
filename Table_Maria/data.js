c65c72429f24af97c9d13ef9ef6300d9d0bc46b2
RaveElevator <51494031+RaveElevator@users.noreply.github.com>
2020-02-12 18:34:43 -0500

    machine_learning

---
+++ Table_Maria/data.js
@@ -0,0 +1,192 @@
+var data =[{
+    Insland_Name: "Rangyal_Island",
+    continent: "Asia",
+    Country: "Thailand",
+    Price: 160000000,
+    Acreage: 110,
+    Lailitude: 7.95,
+    Longitude: 98.44
+
+}, 
+{
+    Insland_Name: "Spectabilis Island",
+    continent: "North america",
+    Country: "Bahamas",
+    Price: 6200000,
+    Acreage: 460,
+    Latitude: 24.35,
+    Longitude: -76.58
+},
+{
+    Insland_Name: "Cave_cay",
+    continent: "North america",
+    Country: "TBahamas",
+    Price: 6000000,
+    Acreage: 22.23,
+    Latitude: -18.75,
+    Longitude: -141.80
+},
+{
+    Insland_Name: "Nengo Nengo Atoll",
+    continent: "Europe",
+    Country: "TBahamasFrance",
+    Price: 55000000,
+    Acreage: 22.23,
+    Latitude: 23.90,
+    Longitude: -76.26
+},
+{
+    Insland_Name: "Kandoo Island",
+    continent: "Asia",
+    Country: "Maldives",
+    Price: "NaN",
+    Acreage: 42,
+    Latitude: 5.85,
+    Longitude: -73.33
+},
+{
+    Insland_Name: "Kanacea Island",
+    continent: "South Pacific Ocean",
+    Country: "Fiji",
+    Price: 260000000,
+    Acreage: 3085,
+    Latitude: -17.23,
+    Longitude: -179.15
+},
+{
+    Insland_Name: "Serenity Beach",
+    continent: "South Pacific Ocean",
+    Country: "Tonga",
+    Price: "NaN",
+    Acreage: 8,
+    Latitude: -19.85,
+    Longitude: -174.41
+},
+{
+    Insland_Name: "Big Scrub Island",
+    continent: "Europe",
+    Country: "United Kigndom",
+    Price: "NaN",
+    Acreage: 170,
+    Latitude: -18.46,
+    Longitude: -64.80
+},
+{
+    Insland_Name: "Tojo Una Island",
+    continent: "Asia",
+    Country: "Indonesia",
+    Price: 61000000,
+    Acreage: 30.89,
+    Latitude: -0.74,
+    Longitude: -121.76
+},
+{
+    Insland_Name: "Pelican Island",
+    continent: "Caribean",
+    Country: "Antigua",
+    Price: 68000000,
+    Acreage: 32,
+    Latitude: 17.10,
+    Longitude: -61.70
+},
+{
+    Insland_Name: "Isla Rancheria",
+    continent: "Cenrtral America",
+    Country: "Panama",
+    Price: NaN,
+    Acreage: 310,
+    Latitude: 8.26,
+    Longitude: -82.42
+},
+{
+    Insland_Name: "Darby Island",
+    continent: "North America",
+    Country: "Bahamas",
+    Price: 46000000,
+    Acreage: 554,
+    Latitude: 23.84,
+    Longitude: -76.22
+},
+{
+    Insland_Name: "Motu Tane",
+    continent: "Europe",
+    Country: "France",
+    Price: 39000000,
+    Acreage: 10,
+    Latitude: -16.44,
+    Longitude: -151.56
+},
+{
+    Insland_Name: "Organica Beach",
+    continent: "Oceania",
+    Country: "Panama",
+    Price: 38000000,
+    Acreage: 14.92,
+    Latitude: -8.30,
+    Longitude: -78.97
+},
+{
+    Insland_Name: "Big Grand Cary",
+    continent: "North America",
+    Country: "Bahamas",
+    Price: 29000000,
+    Acreage: 213,
+    Latitude: 27.22,
+    Longitude: -78.31
+},
+{
+    Insland_Name: "Kanacea Island",
+    continent: "South Pacific",
+    Country: "France",
+    Price: 26000000,
+    Acreage: 30.85,
+    Latitude: -17.26,
+    Longitude: -179.34
+},
+{
+    Insland_Name: "Lighthouse Cary",
+    continent: "North America",
+    Country: "Bahamas",
+    Price: 26000000,
+    Acreage: 7.65,
+    Latitude: 24.76,
+    Longitude: -76.14
+},
+
+{
+    Insland_Name: "Mantana South Seas Plantation",
+    continent: "North America",
+    Country: "Bahamas",
+    Price: 26000000,
+    Acreage: 7.65,
+    Latitude: 24.76,
+    Longitude: -76.47
+},
+{
+    Insland_Name: "Lera Cary",
+    continent: "North America",
+    Country: "Bahamas",
+    Price: 24000000,
+    Acreage: 80,
+    Latitude: 24.76,
+    Longitude: -76.26
+},
+{
+    Insland_Name: "El Nido Peninsula",
+    continent: "Asia",
+    Country: "Philipines",
+    Price: 199000,
+    Acreage: 3,
+    Latitude: 11.10,
+    Longitude: 119.34
+},
+{
+    Insland_Name: "Pebble Island",
+    continent: "Saouth America",
+    Country: "Frankland Island",
+    Price: 119000,
+    Acreage: 26.24,
+    Latitude: -51.28,
+    Longitude: -59.65
+},
+];
